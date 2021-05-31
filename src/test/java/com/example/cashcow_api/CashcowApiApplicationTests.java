29b051cb082abd6250eef19d2ed8722e3f3b2b1f
Vincent Kimutai <vincent@aspira.co.ke>
2021-05-31 15:16:02 +0300

    first commit

---
+++ src/test/java/com/example/cashcow_api/CashcowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cashcow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashcowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
